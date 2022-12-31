b50718bf3d82b09076d2f93e032a0eb81f622c38
Pavol Noha <pavol.noha@gmail.com>
2022-12-31 14:41:27 +0100

    Simple asset switch

--- src/component/AssetSwitch.ts
+++ src/component/AssetSwitch.ts
@@ -4,12 +4,21 @@ import { customElement } from 'lit/decorators.js';
 import { UIGCElement } from './base/UIGCElement';
 
 import './icons/Switch';
+import './icons/Arrow';
 
 @customElement('uigc-asset-switch')
 export class AssetSwitch extends UIGCElement {
   static styles = [
     UIGCElement.styles,
     css`
+      :host([basic]) uigc-icon-arrow {
+        display: block;
+      }
+
+      :host(:not([basic])) uigc-icon-switch {
+        display: block;
+      }
+
       .switch-root {
         width: 43px;
         height: 43px;
@@ -18,11 +27,14 @@ export class AssetSwitch extends UIGCElement {
         align-items: center;
       }
 
-      uigc-icon-switch {
+      uigc-icon-switch,
+      uigc-icon-arrow {
+        display: none;
         transition: var(--uigc-asset-switch-transition);
       }
 
-      .switch-root:hover > uigc-icon-switch {
+      .switch-root:hover > uigc-icon-switch,
+      .switch-root:hover > uigc-icon-arrow {
         cursor: pointer;
         transform: var(--uigc-asset-switch-transform);
       }
@@ -41,6 +53,7 @@ export class AssetSwitch extends UIGCElement {
     return html`
       <div class="switch-root" @click=${this.onSwitchClick}>
         <uigc-icon-switch></uigc-icon-switch>
+        <uigc-icon-arrow></uigc-icon-arrow>
       </div>
     `;
   }

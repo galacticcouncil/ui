f5b9e675694525d9e63b5ebac29e443626846d7f
Pavol Noha <pavol.noha@gmail.com>
2023-09-06 00:06:56 +0200

    HDX bond icon support

--- src/component/logo/AssetLogo.ts
+++ src/component/logo/AssetLogo.ts
@@ -54,6 +54,7 @@ export class AssetLogo extends BaseLogo {
           ['ETH', () => weth],
           ['GLMR', () => moonbeam],
           ['HDX', () => hydraDX],
+          ['HDXb', () => hydraDX],
           ['IBTC', () => ibtc],
           ['INTR', () => interlay],
           ['KAR', () => karura],
